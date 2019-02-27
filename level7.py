92e296bc8f5e87acef6cd9ba1798038ad8e71a44
tschelbs18 <44124549+tschelbs18@users.noreply.github.com>
2019-02-27 14:11:48 -0500

    Add files via upload

--- level7.py
+++ level7.py
@@ -1,38 +1,6 @@
-master_str = "hockey"
+# Oxygen
+# XML parsing? 
+# Based on the image it appears to be darker in the water aka less oxygen?
 
-# It's in the air. Look at the letters.
-
-english_dictionary_file = open("words_alpha.txt")
-english_dictionary = english_dictionary_file.read().splitlines()
-
-def fn_get_letters_dict(word):
-    letters_dict = {}
-    for letter in word:
-        if letter in letters_dict.keys():
-            letters_dict[letter] += 1
-        else:
-            letters_dict[letter] = 1
-    return letters_dict
-    
-def fn_compare_letters_dicts(sub_word, full_word):
-    # This will evaluate words that can be made using letters from full_word
-    test_word = fn_get_letters_dict(sub_word)
-    master_word = fn_get_letters_dict(full_word)
-    
-    for letter in test_word.keys():
-        if letter not in master_word.keys():
-            return False
-        elif test_word[letter] > master_word[letter]:
-            return False
-    return True
-    
-new_words = [] 
-    
-for word in english_dictionary:
-    if fn_compare_letters_dicts(word, master_str):
-        new_words.append(word)
-        
-print(new_words)
-    
-    
+import xml
 
